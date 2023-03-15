de0a44d6eb928216fcdfcff4ed671d3293e39858
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-03-15 19:40:11 +0530

    add neotree config
    Signed-off-by: taskylizard <75871323+taskylizard@users.noreply.github.com>

---
+++ .config/nvim/lua/user/plugins/neo-tree.lua
@@ -0,0 +1,34 @@
+local events = require "neo-tree.events"
+---@param args FileMovedArgs
+local function on_file_remove(args)
+  local ts_clients = vim.lsp.get_active_clients { name = "tsserver" }
+  for _, ts_client in ipairs(ts_clients) do
+    ts_client.request("workspace/executeCommand", {
+      command = "_typescript.applyRenameFile",
+      arguments = {
+        {
+          sourceUri = vim.uri_from_fname(args.source),
+          targetUri = vim.uri_from_fname(args.destination),
+        },
+      },
+    })
+  end
+end
+
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      event_handlers = {
+        {
+          event = events.FILE_MOVED,
+          handler = on_file_remove,
+        },
+        {
+          event = events.FILE_RENAMED,
+          handler = on_file_remove,
+        },
+      },
+    },
+  },
+}
